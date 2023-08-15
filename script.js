180ab2cb06aa1da752ba071f84bdd668c875130e
Shreyash Daundkar <productiveappa@gmail.com>
2023-08-15 06:35:08 +0530

    Converted name of item from text to textNode. Now we can clear input text box after adding element

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ const itemsList = document.querySelector('#items');
 addForm.addEventListener('submit', addItem);
 function addItem(e) {
     e.preventDefault();
-    const listItem = addElement('li', itemsList, item.value, 'list-group-item');
-    const editBtn = addElement('button', listItem, 'Edit', 'btn', 'btn-sm', 'float-right', 'btn-warning', 'ml-2', 'edit');
-    const delBtn = addElement('button', listItem, 'X', 'btn', 'btn-danger',  'btn-sm', 'float-right', 'delete');
+    const listItem = addElement('li', itemsList, document.createTextNode(item.value), 'list-group-item');
+    const editBtn = addElement('button', listItem, document.createTextNode('Edit'), 'btn', 'btn-sm', 'float-right', 'btn-warning', 'ml-2', 'edit');
+    const delBtn = addElement('button', listItem, document.createTextNode('X'), 'btn', 'btn-danger',  'btn-sm', 'float-right', 'delete');
+    item.value = "";
 }
 
 // managing items list operations.
@@ -24,16 +25,23 @@ function itemOperation(e) {
 // Delete items functinality
 
 function deleteItem(element) {
-    const isConfirmed = confirm('Are you sure you want to delete this item?');
+    const isConfirmed = confirm(`Are you sure you want to delete ${element.children[0].value}?`);
     if(isConfirmed) element.remove();
 }
 
+
 // Utility functions
 
-function addElement(element, parent, text,  ...classes) {
+function addElement(element, parent, textNode,  ...classes) {
     const newElement = document.createElement(element);
     classes.forEach(c => newElement.classList.add(c));
-    newElement.innerText = text;
+    newElement.append(textNode);
     parent.append(newElement);
     return newElement;
+}
+
+document.querySelector('#filter').addEventListener('click', see);
+
+function see() {
+    Array.from(itemsList.children).forEach(x => console.log(x.firstChild.textContent));
 }
